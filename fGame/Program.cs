f6da35b48dd1998715ec16701734d625ab1c72a9
demonh1 <demon7987@gmail.com>
2012-11-23 02:30:18 +0400

    new version

--- fGame/Program.cs
+++ fGame/Program.cs
@@ -15,7 +15,7 @@ namespace fGame
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormGame());
         }
     }
 }
